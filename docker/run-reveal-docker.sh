1ef4c337f4dfb5fb12802239bfcb2d4ddb9890f6
Sujee Maniyam <sujee@sujee.net>
2019-02-24 01:49:04 -0800

    DL : adding docker for development [3]

---
+++ docker/run-reveal-docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -z "$1" ] ; then
+    echo "Usage:  $0    <image name>    [optional command]"
+    echo "Missing Docker image id.  exiting"
+    exit -1
+fi
+
+image_id="$1"
+cmd="$2"
+
+
+# mount the current directory at /home/jovyan/work
+this="${BASH_SOURCE-$0}"
+#mydir=$(cd -P -- "$(dirname -- "$this")" && pwd -P)
+mydir=$(pwd -P)
+
+## ports:
+##  1948-1950 : Reveal UI
+
+docker run -it   \
+    -p 1948-1950:1948-1950 \
+    -v"$mydir:/home/ubuntu/work" \
+    "$image_id" \
+    ${cmd}
