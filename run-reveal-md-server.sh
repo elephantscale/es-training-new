f563d0c106d23efb4a359d247acb46e4b0b75986
Sujee Maniyam <sujee@sujee.net>
2019-03-15 21:35:48 -0700

    reveal run scripts

--- run-reveal-md-server.sh
+++ run-reveal-md-server.sh
@@ -2,11 +2,14 @@
 
 port=2000
 
+# choices are : googlecode, atom-one-light
+syntax="googlecode"
+
 ## ES theme
 if [ -f /.dockerenv ]; then
     #echo "I'm inside container"
-    reveal-md --port $port --disable-auto-open --theme assets/css/theme/es.css --template assets/css/es-template.html --highlight-theme  atom-one-light --listing-template assets/css/listing-simple.html .
+    reveal-md --port $port --disable-auto-open --theme assets/css/theme/es.css --template assets/css/es-template.html --highlight-theme  $syntax --listing-template assets/css/listing-simple.html .
 else
     # echo "I'm living in real world!"
-    reveal-md --port $port --watch --theme assets/css/theme/es.css --template assets/css/es-template.html --highlight-theme  atom-one-light --listing-template assets/css/listing-simple.html .
+    reveal-md --port $port --watch --theme assets/css/theme/es.css --template assets/css/es-template.html --highlight-theme  $syntax --listing-template assets/css/listing-simple.html .
 fi
