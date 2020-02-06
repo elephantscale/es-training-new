7201a3b89682ad52b5466ec57a7d954ba1b7382e
Sujee Maniyam <sujee@elephantscale.com>
2020-02-05 17:25:08 -0800

    ds-gcp : outline refactored [1]

--- data-science-gcp/slides/gen.sh
+++ data-science-gcp/slides/gen.sh
@@ -8,9 +8,9 @@
 ##	./gen.sh  pptx pdf
 
 ## generate outline doc pdf
-pandoc ../data-science-on-google-cloud-outline.md  -f markdown -o  ../data-science-on-google-cloud-outline.docx
+pandoc ../google-cloud-for-data-scientists-outline.md  -o  ../google-cloud-for-data-scientists-outline.docx
 
-pandoc ../data-science-on-google-cloud-outline.md  -f markdown  -o ../data-science-on-google-cloud-outline.pdf
+pandoc ../google-cloud-for-data-scientists-outline.md   -o ../google-cloud-for-data-scientists-outline.pdf
 
 ## -d will delete existing files in assembly  folder
 $ES_HOME/utils/reveal-md/slides-assembler-reveal.sh  -d     0-slide-assembly.txt $@
