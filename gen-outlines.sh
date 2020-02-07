e99f74128aaf8b967800c9dac695677870ad9801
Sujee Maniyam <sujee@elephantscale.com>
2020-02-07 08:59:30 -0800

    outline gen script update

--- gen-outlines.sh
+++ gen-outlines.sh
@@ -1,7 +1,7 @@
-#!/bin/bash 
+#!/bin/bash
 
 outlines="
-kafka/Intro-to-Apache-Kafka-outline-3days.md 
+kafka/Intro-to-Apache-Kafka-outline-3days.md
 kafka/Intro-to-Apache-Kafka-outline-4days.md
 data-science-gcp/google-cloud-for-data-scientists-outline.md
 deep-learning/Intro-to-Deep-Learning-With-TensorFlow-and-Keras-outline-2days.md
@@ -12,31 +12,30 @@ output_dir="outlines.out"
 mkdir -p $output_dir
 rm -f $output_dir/*
 
-while read -r outline ; do 
+while read -r outline ; do
     #echo '----'
-    #echo $outline 
-    
-    
-    input_file=$outline 
-    
+    #echo $outline
+
+
+    input_file=$outline
+
     if [ -f "$input_file" ] ; then
-        a=$(basename $input_file)
-        output_file_docx="${a%\.*}.docx"
-        output_file_pdf="${a%\.*}.pdf"
-        
         input_dir=$(dirname $input_file)
-        
+        basename=$(basename $input_file)
+
+        output_file_docx="${basename%\.*}.docx"
+        output_file_pdf="${basename%\.*}.pdf"
+
+
         echo "$input_file --> $input_dir/$output_file_docx"
         pandoc $input_file  -o  "$input_dir/$output_file_docx"
-        
+
         echo "$input_file --> $input_dir/$output_file_pdf"
         pandoc $input_file  -o  "$input_dir/$output_file_pdf"
-        
+
         echo "$input_file --> $output_dir/$output_file_docx"
-        pandoc $input_file  -o  "$output_dir/$output_file_docx"
-        
+        cp "$input_dir/$output_file_docx"  $output_dir/
         echo "$input_file --> $output_dir/$output_file_pdf"
-        pandoc $input_file  -o  "$output_dir/$output_file_pdf"
+        cp "$input_dir/$output_file_pdf"  $output_dir/
     fi
 done <<< "$outlines"
-
