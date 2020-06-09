b334084a7752caaa14d321481e2c24663f5445cb
Sujee Maniyam <sujee@elephantscale.com>
2020-06-08 23:36:18 -0700

    DL : refactor TF gen.sh [1]

---
+++ deep-learning/tf2/gen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## generate outlines
+pandoc  ../deep-learning-with-tensorflow-keras-2days.md -o ../deep-learning-with-tensorflow-keras-2days.docx
+pandoc  ../deep-learning-with-tensorflow-keras-2days.md -o ../deep-learning-with-tensorflow-keras-2days.pdf
+
+
+pandoc ../deep-learning-with-tensorflow-keras-3days.md  -o ../deep-learning-with-tensorflow-keras-3days.docx
+pandoc ../deep-learning-with-tensorflow-keras-3days.md  -o ../deep-learning-with-tensorflow-keras-3days.pdf
+
+
+$ES_HOME/utils/reveal-md/slides-assembler-reveal.sh  -d    0-slide-assembly-DL-tf2-3-days.txt $@
+#$ES_HOME/utils/reveal-md/slides-assembler-reveal.sh  -d    0-slide-assembly-DL-tf2-2-days.txt $@
