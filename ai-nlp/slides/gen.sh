5f249e75412fa3e0192b8174e4964513437c0e6f
Sujee Maniyam <sujee@elephantscale.com>
2020-10-06 16:46:01 -0700

    nlp : reset of gen.sh

--- ai-nlp/slides/gen.sh
+++ ai-nlp/slides/gen.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 ### ======  regular ============================================
-#pandoc ../../course-outlines/AI-for-NLP-Intro.md -o AI-for-NLP-Intro.docx
-#pandoc ../../course-outlines/AI-for-NLP-Intro.md -o AI-for-NLP-Intro.pdf
-#$ES_HOME/utils/reveal-md/slides-assembler-reveal.sh    -d 0-slide-order.txt   $@
+pandoc ../../course-outlines/AI-for-NLP-Intro.md -o AI-for-NLP-Intro.docx
+pandoc ../../course-outlines/AI-for-NLP-Intro.md -o AI-for-NLP-Intro.pdf
+$ES_HOME/utils/reveal-md/slides-assembler-reveal.sh    -d 0-slide-order.txt   $@
 ### ======  end regular ============================================
 
 
-### ======= donis =======
-pandoc   ../../course-outlines/donis-marshal/text-analytics-using-machine-learning.md    -o text-analytics-using-machine-learning.pdf
-pandoc   ../../course-outlines/donis-marshal/text-analytics-using-machine-learning.md    -o text-analytics-using-machine-learning.docx
-$ES_HOME/utils/reveal-md/slides-assembler-reveal.sh    -d 0-slide-order-donis-half-day.txt   $@
-### ======= end donis =======
+### ======= half day =======
+# pandoc   ../../course-outlines/donis-marshal/text-analytics-using-machine-learning.md    -o text-analytics-using-machine-learning.pdf
+# pandoc   ../../course-outlines/donis-marshal/text-analytics-using-machine-learning.md    -o text-analytics-using-machine-learning.docx
+# $ES_HOME/utils/reveal-md/slides-assembler-reveal.sh    -d 0-slide-order-donis-half-day.txt   $@
+### ======= end half day =======
