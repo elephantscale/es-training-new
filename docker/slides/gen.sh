7c6f37e9f54f322a447c31dbc4ab117ec8d7ddbe
Sujee Maniyam <sujee@elephantscale.com>
2021-09-11 03:27:17 -0700

    docker : gen.sh

--- docker/slides/gen.sh
+++ docker/slides/gen.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # To generate slides
-$ES_HOME/utils/presentations/slides-assembler.sh   slide-order.txt
+# $ES_HOME/utils/presentations/slides-assembler.sh   slide-order.txt
 
+$ES_HOME/utils/reveal-md/slides-assembler-reveal.sh  -d   0-slide-order.txt $@
