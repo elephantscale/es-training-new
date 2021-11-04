80e637eeb2f7afb3e046c4e354ad94ebc46c1fff
Sujee Maniyam <sujee@elephantscale.com>
2021-11-04 07:21:11 -0700

    scala : gen script [0.5]

---
+++ scala/slides/gen.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+## to generate pdf 
+##	./gen.sh  pdf
+## to generate pptx
+##	./gen.sh  pptx
+## to generate pptx and pdf
+##	./gen.sh  pptx pdf
+
+## 3 days 
+(cd ../../course-outlines; pandoc scala.md  -o ../scala/scala.docx)
+(cd ../../course-outlines; pandoc scala.md  -o ../scala/scala.pdf)
+
+
+## 3 days
+$ES_HOME/utils/reveal-md/slides-assembler-reveal.sh  -d   0-slide-assembly-3-days.txt $@
+
