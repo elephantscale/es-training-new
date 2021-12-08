d189a495da2e99acfb8e53a1bbb46d76ab788e57
Tim Fox <tfox@createksolutions.com>
2021-12-08 09:02:05 -0500

    Added python slides

--- python/slides/gen-custom.sh
+++ python/slides/gen-custom.sh
@@ -2,4 +2,4 @@
 
 
 ## Custom for Kim Morello
-$ES_HOME/utils/reveal-md/slides-assembler-reveal.sh  -d   slide-order-kim-TTPS4876.txt  $@
+$ES_HOME/utils/reveal-md/slides-assembler-reveal.sh  -d   slide-order-python-language-advanced.txt  $@
