aa87b0e589d5a014c7b7e29351485b080215fab0
Sujee Maniyam <sujee@elephantscale.com>
2022-01-27 22:03:13 -0800

    kafka : custom outline

--- kafka/slides/gen-custom.sh
+++ kafka/slides/gen-custom.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-## 2 days
-$ES_HOME/utils/reveal-md/slides-assembler-reveal.sh  -d   0-slide-assembly-2-days-triveratech.txt $@
+# Innovation Software - Adv Kafka - 3 days
+$ES_HOME/utils/reveal-md/slides-assembler-reveal.sh  -d   0-slide-assembly-3-days-innovation-software.txt $@
 
 
