645cb661ce60260e02224829d80f9a312794a3ae
Sujee Maniyam <sujee@elephantscale.com>
2023-02-06 12:51:38 -0800

    python datascience slide order and getting started

--- python/slides/gen.sh
+++ python/slides/gen.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-$ES_HOME/utils/presentations/slides-assembler.sh   slide-order-python-complete.txt
+# $ES_HOME/utils/presentations/slides-assembler.sh   slide-order-python-complete.txt
+
+# $ES_HOME/utils/reveal-md/slides-assembler-reveal.sh  -d   slide-order.txt  $@
+
+$ES_HOME/utils/reveal-md/slides-assembler-reveal.sh  -d   0-slide-order-data-analytics-with-python.txt  $@
