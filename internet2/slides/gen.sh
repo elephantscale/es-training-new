4f5be134addcf6398e1d2be087221b3f86d2a9be
Sujee Maniyam <sujee@elephantscale.com>
2023-03-19 22:52:13 -0700

    updated gen.sh

--- internet2/slides/gen.sh
+++ internet2/slides/gen.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-# To generate slides
+# To generate PDF slides
 $ES_HOME/utils/reveal-md/slides-assembler-reveal.sh   0-slide-order.txt $@
+
+
+# To generate PPTX slides
+$ES_HOME/utils/presentations/slides-assembler2.sh   0-slide-order.txt $@
