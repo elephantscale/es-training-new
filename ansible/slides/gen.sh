f878d5ab8df098e1a44c8a8a10e915611ffe3451
Mark Kerzner <mark@elephantscale.com>
2025-01-06 22:25:13 -0600

    instrcutions

--- ansible/slides/gen.sh
+++ ansible/slides/gen.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# NOTE: Kim wants to generate one side deck
+# For that, generate slide deck according to the list, then glue them together
 # To generate slides
 $ES_HOME/utils/presentations/slides-assembler.sh slide-list.txt
